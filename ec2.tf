524b01bba291688be4044eec8ae1f8d76a12c1ca
nissiambe@yahoo.com <nissiambe@yahoo.com>
2022-11-14 20:41:41 -0500

    correction

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami           = var.AMI_ID
   instance_type = var.INSTANCE_TYPE
   monitoring = true
-  ebs_optimized = true
+  #ebs_optimized = true
   root_block_device {
   encrypted     = true
   }
